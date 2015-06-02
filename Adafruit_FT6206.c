a10b583a9d71c79d8efb2f3fafceb5733ec63131
Patrick Shields <patrick@randomwire.biz>
2015-06-02 01:37:37 -0400

    Updates to fix compilation errors

--- Adafruit_FT6206.c
+++ Adafruit_FT6206.c
@@ -25,12 +25,15 @@
 
 #include <Adafruit_FT6206.h>
 #include "bsp_i2c.h"
+#include "board.h"
+#include "sensor.h"
+#include <stdlib.h>
 
 // Private member variables from class become static variables.
 static uint8_t touches = 0;
 static uint16_t touchX[2];
 static uint16_t touchY[2];
-static uint16_t touchID[2];
+static uint16_t touchWeight[2];
 
 
 
@@ -40,8 +43,7 @@ static uint16_t touchID[2];
     @brief  Setups the HW
 */
 /**************************************************************************/
-uint8_t Adafruit_FT6206_begin(uint8_t threshhold) {
-  Wire.begin();
+uint8_t Adafruit_FT6206_setup(uint8_t threshhold) {
 
   // change threshhold to be higher/lower
   Adafruit_FT6206_writeRegister8(FT6206_REG_THRESHHOLD, threshhold);
@@ -67,27 +69,27 @@ uint8_t Adafruit_FT6206_begin(uint8_t threshhold) {
 // DONT DO THIS - REALLY - IT DOESNT WORK
 void Adafruit_FT6206_autoCalibrate(void) {
  Adafruit_FT6206_writeRegister8(FT6206_REG_MODE, FT6206_REG_FACTORYMODE);
- delay(100);
+ delay_ms(100);
  //Serial.println("Calibrating...");
  Adafruit_FT6206_writeRegister8(FT6206_REG_CALIBRATE, 4);
- delay(300);
+ delay_ms(300);
  for (uint8_t i = 0; i < 100; i++) {
    uint8_t temp;
    temp = Adafruit_FT6206_readRegister8(FT6206_REG_MODE);
-   Serial.println(temp, HEX);
+   //Serial.println(temp, HEX);
    //return to normal mode, calibration finish 
    if (0x0 == ((temp & 0x70) >> 4))
      break;
  }
- delay(200);
+ delay_ms(200);
  //Serial.println("Calibrated");
- delay(300);
+ delay_ms(300);
  Adafruit_FT6206_writeRegister8(FT6206_REG_MODE, FT6206_REG_FACTORYMODE);
- delay(100);
+ delay_ms(100);
  Adafruit_FT6206_writeRegister8(FT6206_REG_CALIBRATE, 5);
- delay(300);
+ delay_ms(300);
  Adafruit_FT6206_writeRegister8(FT6206_REG_MODE, FT6206_REG_WORKMODE);
- delay(300);
+ delay_ms(300);
 }
 
 
@@ -100,12 +102,12 @@ uint8_t Adafruit_FT6206_touched(void) {
 
 /*****************************/
 
-void Adafruit_FT6206_readData(uint16_t *x, uint16_t *y) {
+void Adafruit_FT6206_readData(uint16_t *x, uint16_t *y, uint16_t *z) {
 
     uint8_t i2cdat[16];
     uint8_t data = 0;
-    if (bspI2cSelect(REGISTER_INTERFACE_I2C0,FT6206_ADDR)) {
-        bspI2cWrite(data,1);
+    if (bspI2cSelect(0,FT6206_ADDR)) {
+        bspI2cWrite(&data,1);
         bspI2cRead(i2cdat,16);
         bspI2cDeselect(); // Release I2C driver back to other tasks
     } else {
@@ -154,34 +156,34 @@ void Adafruit_FT6206_readData(uint16_t *x, uint16_t *y) {
         touchY[i] = i2cdat[0x05 + i*6] & 0x0F;
         touchY[i] <<= 8;
         touchY[i] |= i2cdat[0x06 + i*6];
-        touchID[i] = i2cdat[0x05 + i*6] >> 4;
+        touchWeight[i] = i2cdat[0x07 + i*6];
     }
     /*
     Serial.println();
     for (uint8_t i=0; i<touches; i++) {
-      Serial.print("ID #"); Serial.print(touchID[i]); Serial.print("\t("); Serial.print(touchX[i]);
+    Serial.print("Weight: "); Serial.print(touchWeight[i]); Serial.print("\t("); Serial.print(touchX[i]);
       Serial.print(", "); Serial.print(touchY[i]);
       Serial.print (") ");
     }
     Serial.println();
     */
-    *x = touchX[0]; *y = touchY[0];
+    *x = touchX[0]; *y = touchY[0]; *z = touchWeight[0];
 }
 
 TS_Point Adafruit_FT6206_getPoint(void) {
-  uint16_t x, y;
-  uint8_t z;
-  readData(&x, &y);
-  return TS_Point_Init(x, y, 1);
+  uint16_t x, y, z;
+  //uint8_t z;
+  Adafruit_FT6206_readData(&x, &y, &z);
+  return TS_Point_Init(x, y, z);
 }
 
 
 uint8_t Adafruit_FT6206_readRegister8(uint8_t reg) {
     uint8_t x ;
     // use i2c
-    if (bspI2cSelect(REGISTER_INTERFACE_I2C0,FT6206_ADDR)) {
-        bspI2cWrite(reg,1);
-        bspI2cRead(x,1);
+    if (bspI2cSelect(0,FT6206_ADDR)) {
+        bspI2cWrite(&reg,1);
+        bspI2cRead(&x,1);
         bspI2cDeselect(); // Release I2C driver back to other tasks
     } else {
         return 0;
@@ -196,7 +198,7 @@ uint8_t Adafruit_FT6206_readRegister8(uint8_t reg) {
 void Adafruit_FT6206_writeRegister8(uint8_t reg, uint8_t val) {
     // use i2c
     uint8_t i2cData[2] = {reg,val};
-    if (bspI2cSelect(REGISTER_INTERFACE_I2C0,FT6206_ADDR)) {
+    if (bspI2cSelect(0,FT6206_ADDR)) {
         bspI2cWrite(i2cData,2);
         bspI2cDeselect(); 
     }
@@ -213,7 +215,7 @@ TS_Point TS_Point_Init(int16_t x0, int16_t y0, int16_t z0) {
 }
 
 TS_Point* TS_Point_New(int16_t x0, int16_t y0, int16_t z0) {
-  TS_Point* point = malloc(sizeof(TS_Point));
+  TS_Point* point = (TS_Point*) malloc(sizeof(TS_Point));
   point->x = x0;
   point->y = y0;
   point->z = z0;
--- Adafruit_FT6206.h
+++ Adafruit_FT6206.h
@@ -17,6 +17,7 @@
 #ifndef _ADAFRUIT_FT6206_H
 #define _ADAFRUIT_FT6206_H
 
+#include <stdint.h>
 
 #define FT6206_ADDR           0x38
 #define FT6206_G_FT5201ID     0xA8
@@ -47,17 +48,17 @@ struct stTS_Point {
 typedef struct stTS_Point TS_Point;
 
 TS_Point TS_Point_Init(int16_t x, int16_t y, int16_t z);
-*TS_Point TS_Point_New(int16_t x, int16_t y, int16_t z);
+TS_Point* TS_Point_New(int16_t x, int16_t y, int16_t z);
 uint8_t TS_Point_notEqual(TS_Point *p1, TS_Point *p2);
 uint8_t TS_Point_equals(TS_Point *p1, TS_Point *p2);
 
 
-uint8_t Adafruit_FT6206_begin(uint8_t thresh = FT6206_DEFAULT_THRESSHOLD);  
+uint8_t Adafruit_FT6206_setup(uint8_t thresh);  
 
 void Adafruit_FT6206_writeRegister8(uint8_t reg, uint8_t val);
 uint8_t Adafruit_FT6206_readRegister8(uint8_t reg);
 
-void Adafruit_FT6206_readData(uint16_t *x, uint16_t *y);
+void Adafruit_FT6206_readData(uint16_t *x, uint16_t *y, uint16_t *z);
 void Adafruit_FT6206_autoCalibrate(void); 
 
 uint8_t Adafruit_FT6206_touched(void);
